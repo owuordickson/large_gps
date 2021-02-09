3590acfd3dfd21dcc6f6ccb6a56b72d688c694fc
owuordickson <owuordickson@gmail.com>
2021-02-09 23:23:29 +0300

    added

--- src/ant_colony/aco_grad_h5.py
+++ src/ant_colony/aco_grad_h5.py
@@ -191,16 +191,21 @@ class GradACO:
             seg_order = np.argsort(-seg_sum)
             bin_arr = bin_data[:, 2]
             temp_bin = bin_arr[0][seg_order[0]]
-            print(bin_data[0][0])
+            bin_sum = 0
+            gi = GI(bin_data[0][0][0], bin_data[0][0][1].decode())
+            gen_pattern.add_gradual_item(gi)
+            print(gi.to_string())
             for i in seg_order:
                 for j in range(1, len(bin_arr)):
                     temp_bin = np.multiply(temp_bin, bin_arr[j][i])
-                    print(temp_bin)
-                    supp = float(np.sum(temp_bin)) / float(n * (n - 1.0) / 2.0)
+                    bin_sum += np.sum(temp_bin)
+                    supp = float(bin_sum) / float(n * (n - 1.0) / 2.0)
                     print("Support: " + str(supp))
                     if supp >= min_supp:
-                        print(bin_data[j][0])
-
+                        gi = GI(bin_data[j][0][0], bin_data[j][0][1].decode())
+                        gen_pattern.add_gradual_item(gi)
+                        gen_pattern.set_support(supp)
+                        print(gi.to_string())
             # print(seg_sum)
             # print(seg_order)
             # print(bin_arr)
@@ -214,6 +219,7 @@ class GradACO:
             # print(self.d_set.valid_bins[:, 0])
             # print(arg)
             # print(len(arg))
+        print(gen_pattern.to_string())
         if len(gen_pattern.gradual_items) <= 1:
             return pattern
         else:
