8a57e976fe57baf118d67f25e7f088d23a13a3a1
owuordickson <owuordickson@gmail.com>
2021-02-17 16:56:54 +0300

    1st working version of hdf5

--- src/ant_colony/aco_grad_h5.py
+++ src/ant_colony/aco_grad_h5.py
@@ -10,6 +10,7 @@
 Breath-First Search for gradual patterns (ACO-GRAANK)
 
 """
+import os
 import h5py
 import numpy as np
 from itertools import combinations
@@ -25,97 +26,6 @@ class GradACO:
         self.e_factor = 0.5  # evaporation factor
         self.iteration_count = 0
         self.d, self.attr_keys = self.generate_d()  # distance matrix (d) & attributes corresponding to d
-
-    @staticmethod
-    def check_anti_monotony(lst_p, pattern, subset=True):
-        result = False
-        if subset:
-            for pat in lst_p:
-                result1 = set(pattern.get_pattern()).issubset(set(pat.get_pattern()))
-                result2 = set(pattern.inv_pattern()).issubset(set(pat.get_pattern()))
-                if result1 or result2:
-                    result = True
-                    break
-        else:
-            for pat in lst_p:
-                result1 = set(pattern.get_pattern()).issuperset(set(pat.get_pattern()))
-                result2 = set(pattern.inv_pattern()).issuperset(set(pat.get_pattern()))
-                if result1 or result2:
-                    result = True
-                    break
-        return result
-
-    @staticmethod
-    def is_duplicate(pattern, lst_winners, lst_losers):
-        for pat in lst_losers:
-            if set(pattern.get_pattern()) == set(pat.get_pattern()) or \
-                    set(pattern.inv_pattern()) == set(pat.get_pattern()):
-                return True
-        for pat in lst_winners:
-            if set(pattern.get_pattern()) == set(pat.get_pattern()) or \
-                    set(pattern.inv_pattern()) == set(pat.get_pattern()):
-                return True
-        return False
-
-    # ADDED TEST CODES
-    def validate_gp_wait(self, pattern):
-        # pattern = [('2', '+'), ('4', '+')]
-        n = self.d_set.row_count
-        min_supp = self.d_set.thd_supp
-        seg_count = 0
-        gen_pattern = GP()
-        arg = np.argwhere(np.isin(self.d_set.valid_bins[:, 0], pattern.get_np_pattern()))
-        if len(arg) >= 2:
-            bin_data = self.d_set.valid_bins[arg.flatten()]
-            seg_sum = np.sum(bin_data[:, 1], axis=0)
-            seg_order = np.argsort(-seg_sum)
-            bin_arr = bin_data[:, 2]
-
-            # print(seg_order)
-            # print(bin_data)
-            # print("\n")
-            # print(bin_arr)
-            # temp_bin = bin_arr[0][seg_order[0]]
-            bin_sum = 0
-            for i in seg_order:
-                curr_bin = None
-                gi_1 = None
-                temp_sum = 0
-                for j in range(len(bin_arr)):
-                    if curr_bin is None:
-                        curr_bin = bin_arr[j][i]
-                        gi_1 = GI(bin_data[j][0][0], bin_data[j][0][1].decode())
-                    else:
-                        temp_bin = np.multiply(curr_bin, bin_arr[j][i])
-                        temp_sum = np.sum(temp_bin)
-                        curr_bin = np.copy(temp_bin)
-
-                        # bin_sum += temp_sum
-                        # supp = float(bin_sum + temp_sum) / float(n * (n - 1.0) / 2.0)  # TO BE REMOVED
-                        # print(temp_bin)
-                        # print("\n")
-                        # print(str(i) + " -- sum: " + str(temp_sum) + ' | total: ' + str(bin_sum))
-                        # print("Support: " + str(supp))
-
-                        if not gen_pattern.contains(gi_1):
-                            gen_pattern.add_gradual_item(gi_1)
-                        gi_2 = GI(bin_data[j][0][0], bin_data[j][0][1].decode())
-                        if not gen_pattern.contains(gi_2):
-                            # print(gi_2.to_string())
-                            # print(gen_pattern.to_string())
-                            gen_pattern.add_gradual_item(gi_2)
-                        supp = float(bin_sum + temp_sum) / float(n * (n - 1.0) / 2.0)
-                        gen_pattern.set_support(supp)
-                bin_sum += temp_sum
-                seg_count += 1
-                supp = float(bin_sum) / float(n * (n - 1.0) / 2.0)
-                if supp >= min_supp:
-                    # print("stopped at: " + str(seg_count))
-                    break
-        if len(gen_pattern.gradual_items) <= 1:
-            return pattern
-        else:
-            return gen_pattern
 
     def generate_d(self):
         # 1a. Retrieve/Generate distance matrix (d)
@@ -268,87 +178,77 @@ class GradACO:
         bin_grps = [h5f[grp_name + k] for k in bin_keys]
 
         if len(bin_grps) >= 2:
-            gen_pattern = self.bin_and(bin_keys, bin_grps)
+            h5_path = 'temp.h5'
+            h5f_1 = h5py.File(h5_path, 'w')
+            h5f.copy((grp_name + bin_keys[0]), h5f_1)
+            main_grp = h5f_1[h5f_1.name][bin_keys[0]]['bins']
+            # print(main_grp['bins']['0'][:])
+            gen_pattern = self.bin_and(bin_keys, bin_grps, main_grp)
+            h5f_1.close()
 
+        h5f.close()
         if len(gen_pattern.gradual_items) <= 1:
             return pattern
         else:
             return gen_pattern
 
-    def bin_and(self, keys, grps):
+    def bin_and(self, keys, grps, m_grp):
         n = self.d_set.attr_size
         min_supp = self.d_set.thd_supp
         pattern = GP()
 
         gi = GI.parse_gi(keys[0])
         pattern.add_gradual_item(gi)
-        bin_1 = grps[0]['bins']
-        main_bin = [bin_1[str(x)][:] for x in range(self.d_set.seg_count)]
+        # bin_1 = grps[0]['bins']
+        # main_bin = [bin_1[str(x)][:] for x in range(self.d_set.seg_count)]
         for i in range(len(keys)):
             if i == 0:
                 continue
             bin_2 = grps[i]['bins']
             # temp_bin = [np.multiply(temp_bin[k], bin_2[str(k)][:]) for k in range(self.d_set.seg_count)]
-            temp_bin = []
+            # temp_bin = []
             bin_sum = 0
             for k in range(self.d_set.seg_count):
-                arr = np.multiply(main_bin[k], bin_2[str(k)][:])
-                bin_sum += np.sum(arr)
-                temp_bin.append(arr)
+                m_grp[str(k)][...] = np.multiply(m_grp[str(k)][:], bin_2[str(k)][:])
+                bin_sum += np.sum(m_grp[str(k)][:])
+                # temp_bin.append(arr)
             supp = float(bin_sum) / float(n * (n - 1.0) / 2.0)
             if supp >= min_supp:
-                main_bin = temp_bin
+                # main_bin = temp_bin
                 gi = GI.parse_gi(keys[i])
                 pattern.add_gradual_item(gi)
                 pattern.set_support(supp)
         # print(str(pattern.to_string()) + ' : ' + str(pattern.support))
+        # h5f_1.close()
         return pattern
 
-    def validate_old(self, pattern):
-        min_supp = self.d_set.thd_supp
-        gen_pattern = GP()
-        arg = np.argwhere(np.isin(self.d_set.valid_bins[:, 0], pattern.get_np_pattern()))
-        if len(arg) >= 2:
-            bin_data = self.d_set.valid_bins[arg.flatten()]
-            segments = np.stack(bin_data[:, 1])
-            large_col_sum = segments.sum(axis=0)
-            large_rows = np.argsort(-segments[:, large_col_sum.argmax()])
-            bin_arr = None
-            for idx in large_rows:
-                if bin_arr is None:
-                    bin_arr = np.array([bin_data[idx][2], None], dtype=object)
-                    gi = GI(bin_data[idx][0][0], bin_data[idx][0][1].decode())
-                    gen_pattern.add_gradual_item(gi)
-                    continue
-                else:
-                    bin_arr[1] = bin_data[idx][2]
-                    temp_bin, supp = self.bin_and(bin_arr)
-                    if supp >= min_supp:
-                        bin_arr[0] = temp_bin
-                        gi = GI(bin_data[idx][0][0], bin_data[idx][0][1].decode())
-                        gen_pattern.add_gradual_item(gi)
-                        gen_pattern.set_support(supp)
-
-        if len(gen_pattern.gradual_items) <= 1:
-            return pattern
+    @staticmethod
+    def check_anti_monotony(lst_p, pattern, subset=True):
+        result = False
+        if subset:
+            for pat in lst_p:
+                result1 = set(pattern.get_pattern()).issubset(set(pat.get_pattern()))
+                result2 = set(pattern.inv_pattern()).issubset(set(pat.get_pattern()))
+                if result1 or result2:
+                    result = True
+                    break
         else:
-            return gen_pattern
+            for pat in lst_p:
+                result1 = set(pattern.get_pattern()).issuperset(set(pat.get_pattern()))
+                result2 = set(pattern.inv_pattern()).issuperset(set(pat.get_pattern()))
+                if result1 or result2:
+                    result = True
+                    break
+        return result
 
-    def bin_and_old(self, bins):
-        n = self.d_set.row_count
-        bin_1 = bins[0]
-        bin_2 = bins[1]
-        temp_bin = []
-        bin_sum = 0
-        for i in range(len(bin_1)):
-            prod = bin_1[i] * bin_2[i]
-            temp_bin.append(prod)
-            bin_sum += np.sum(prod)
-            # print(str(bin_1[i]) + ' x ' + str(bin_2[i]) + '\n')
-            # print(temp_bin)
-            # print("***")
-        temp_bin = np.array(temp_bin, dtype=object)
-        # print(temp_bin)
-        # print(np.sum(temp_bin))
-        supp = float(bin_sum) / float(n * (n - 1.0) / 2.0)
-        return temp_bin, supp
+    @staticmethod
+    def is_duplicate(pattern, lst_winners, lst_losers):
+        for pat in lst_losers:
+            if set(pattern.get_pattern()) == set(pat.get_pattern()) or \
+                    set(pattern.inv_pattern()) == set(pat.get_pattern()):
+                return True
+        for pat in lst_winners:
+            if set(pattern.get_pattern()) == set(pat.get_pattern()) or \
+                    set(pattern.inv_pattern()) == set(pat.get_pattern()):
+                return True
+        return False
