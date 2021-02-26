1aadaf43e838bf7905eb4f1264f4208ff43f821e
owuordickson <owuordickson@gmail.com>
2021-02-26 09:17:42 +0300

    ACO_GRAD v7 - chunking CSV file reads

--- src/algorithms/common/dataset_v7.py
+++ src/algorithms/common/dataset_v7.py
@@ -16,6 +16,8 @@ Changes
 
 """
 import csv
+import gc
+
 import numpy as np
 import pandas as pd
 
@@ -28,25 +30,37 @@ class Dataset:
         self.equal = eq
         self.chunk_count = chunks
         self.titles = Dataset.read_csv_header(file_path)
-        self.col_count = 0  # TO BE UPDATED
+        self.col_count = self.titles.size  # TO BE UPDATED
         self.row_count = 0  # TO BE UPDATED
 
     def init_gp_attributes(self):
-        print(self.titles)
+        # print(self.print_header())
+        self.read_csv_data()
         pass
 
     def read_csv_data(self):
-        df = pd.read_csv(self.csv_file)
+        if self.titles.size > 0:
+            df = pd.read_csv(self.csv_file, sep="[;,' '\t]", engine='python')
+        else:
+            df = pd.read_csv(self.csv_file, sep="[;,' '\t]", engine='python', header=None)
+        print(df)
+
+    def print_header(self):
+        str_header = "Header Columns/Attributes\n-------------------------\n"
+        for txt in self.titles:
+            try:
+                str_header += (str(txt.key) + '. ' + str(txt.value.decode()) + '\n')
+            except AttributeError:
+                try:
+                    str_header += (str(txt[0]) + '. ' + str(txt[1].decode()) + '\n')
+                except IndexError:
+                    str_header += (str(txt) + '\n')
+        return str_header
 
     @staticmethod
     def read_csv_header(file):
         try:
-            with open(file, 'r') as f:
-                dialect = csv.Sniffer().sniff(f.readline(), delimiters=";,' '\t")
-                f.seek(0)
-                reader = csv.reader(f, dialect)
-                header_row = next(reader)
-                f.close()
+            header_row = pd.read_csv(file, sep="[;,' '\t]", engine='python', nrows=0).columns.tolist()
 
             if len(header_row) <= 0:
                 print("CSV file is empty!")
@@ -55,15 +69,17 @@ class Dataset:
                 print("Header titles fetched from CSV file")
                 # 2. Get table headers
                 if header_row[0].replace('.', '', 1).isdigit() or header_row[0].isdigit():
-                    titles = np.array([])
+                    titles = np.arange(len(header_row))
                 else:
                     if header_row[1].replace('.', '', 1).isdigit() or header_row[1].isdigit():
-                        titles = np.array([])
+                        titles = np.arange(len(header_row))
                     else:
                         # titles = self.convert_data_to_array(data, has_title=True)
                         keys = np.arange(len(header_row))
                         values = np.array(header_row, dtype='S')
                         titles = np.rec.fromarrays((keys, values), names=('key', 'value'))
+                del header_row
+                gc.collect()
                 return titles
         except Exception as error:
             print("Unable to read 1st line of CSV file")
--- src/init_acograd_v6.py
+++ src/init_acograd_v6.py
@@ -37,18 +37,14 @@ def init_algorithm(f_path, min_supp, cores, chunks=5):
         d_set = ac.d_set
         wr_line = "Algorithm: ACO-GRAANK (6.0)\n"
         wr_line += "No. of (dataset) attributes: " + str(ac.d_set.col_count) + '\n'
-        wr_line += "No. of (dataset) tuples: " + str(ac.d_set.row_count) + '\n'
+        wr_line += "No. of (dataset) objects: " + str(ac.d_set.row_count) + '\n'
         wr_line += "Minimum support: " + str(min_supp) + '\n'
         wr_line += "Number of cores: " + str(num_cores) + '\n'
         wr_line += "Number of patterns: " + str(len(list_gp)) + '\n'
         wr_line += "Number of chunks: " + str(chunks) + '\n'
         wr_line += "Number of iterations: " + str(ac.iteration_count) + '\n\n'
 
-        for txt in d_set.titles:
-            try:
-                wr_line += (str(txt.key) + '. ' + str(txt.value.decode()) + '\n')
-            except AttributeError:
-                wr_line += (str(txt[0]) + '. ' + str(txt[1].decode()) + '\n')
+        wr_line += ac.d_set.print_header()
 
         wr_line += str("\nFile: " + f_path + '\n')
         wr_line += str("\nPattern : Support" + '\n')
